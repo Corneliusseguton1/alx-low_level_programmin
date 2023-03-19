4cef418c1ae195ad3ae436dfe3c413634d53b486
Corneliusseguton1 <segutoncornelius@gmail.com>
2023-03-19 08:10:36 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+*main - main function
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+	        putchar(i);
+	for (i = 'A' ; i <= 'z' ; i++)
+	putchar('\n');
+        return (0);
+}	
