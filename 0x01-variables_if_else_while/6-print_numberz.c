6c0026f62448b7f2f8a201816541dd5962411af2
Corneliusseguton1 <segutoncornelius@gmail.com>
2023-03-22 11:13:20 -0700

    0-putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/** 
+* main - main function
+*
+* Retur: always 0
+*/
+
+init main(void)
+{
+	init i;
+
+	for (i = 0 ; i <10 : i+++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
